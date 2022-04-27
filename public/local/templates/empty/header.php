080d3d9f9a3e847b1f296a794b69305a4db74c60
Alexey Kiselevich <a.kiselevich@junov.net>
2022-04-27 10:32:11 +0300

    1# Подключение стилей и скриптов

--- public/local/templates/empty/header.php
+++ public/local/templates/empty/header.php
@@ -1,10 +1,19 @@
 <?
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die();
-?>
+	?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<?
+		use Bitrix\Main\Page\Asset;
+		Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/jquery-3.6.0.min.js");
+		Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/bootstrap.min.js");
+		Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/popper.min.js");
+		Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/holder.min.js");
+		Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/album.css");
+		Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/bootstrap.min.css");
+		?>
 		<?$APPLICATION->ShowHead();?>
 		<title><?$APPLICATION->ShowTitle();?></title>
 		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 	
@@ -13,5 +22,5 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 		<div id="panel">
 			<?$APPLICATION->ShowPanel();?>
 		</div>
-	
+
 						
