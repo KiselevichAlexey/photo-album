4bb5da498f057c13cbbe33f65d811f7b6d1ac9a0
Alexey Kiselevich <a.kiselevich@junov.net>
2022-04-28 08:03:08 +0300

    2# Подключена верстка header и footer

--- public/local/templates/empty/footer.php
+++ public/local/templates/empty/footer.php
@@ -2,5 +2,14 @@
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die();
 ?>
+<footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
 	</body>
 </html>
