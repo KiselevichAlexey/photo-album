238d91e18ddade1862ee49e9e75ea65186523d31
Alexey Kiselevich <a.kiselevich@junov.net>
2022-05-04 15:06:10 +0300

    2# Скопирован шаблон фотоголлереи

--- public/urlrewrite.php
+++ public/urlrewrite.php
@@ -1,5 +1,13 @@
 <?php
 $arUrlRewrite=array (
+  1 => 
+  array (
+    'CONDITION' => '#^/albums/#',
+    'RULE' => '',
+    'ID' => 'bitrix:photo',
+    'PATH' => '/albums/index.php',
+    'SORT' => 100,
+  ),
   0 => 
   array (
     'CONDITION' => '#^/rest/#',
